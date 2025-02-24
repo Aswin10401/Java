022b0bfbc602f7ae7e1bd5582bfd704202a2e24c
Aswin <aswinsv2001@gmail.com>
2025-02-24 10:43:22 +0530

    Add SpringJDBCDemo project files

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name , tech) values (101, 'kiran','blockchain');
+insert into alien (id, name , tech) values (102, 'Harsh','AI');
+insert into alien (id, name , tech) values (103, 'Sushil','IoT');
---
+++ target/classes/schema.sql
@@ -0,0 +1,4 @@
+create table alien (
+    id int primary key,
+    name varchar(30),
+    tech varchar(20));
